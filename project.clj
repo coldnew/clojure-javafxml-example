ffe1a68d3308fe32f2f5e1e1a60bebca0abc052f
coldnew <coldnew.tw@gmail.com>
2013-06-01 18:15:20 +0800

    project.clj: add javafxrt

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject clojure-javafxml-example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "clojure javafxml example"
+  :url "https://github.com/coldnew/clojure-javafxml-example"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [local.oracle/javafxrt "2.2.21"]]
   :main clojure-javafxml-example.core)
