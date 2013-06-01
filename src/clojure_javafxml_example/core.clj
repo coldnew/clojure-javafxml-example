423d1807444c0767644b4f93398f41470a670988
coldnew <coldnew.tw@gmail.com>
2013-06-01 18:32:05 +0800

    core.clj: add function to load window.fxml at startup

--- src/clojure_javafxml_example/core.clj
+++ src/clojure_javafxml_example/core.clj
@@ -1,9 +1,20 @@
 (ns clojure-javafxml-example.core
-  (:gen-class))
+  (:import (javafx.application Application)
+           (javafx.fxml FXMLLoader)
+           (javafx.scene Scene)
+           (javafx.stage Stage)))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+(gen-class
+ :name clojure-javafxml-example.core
+ :extends javafx.application.Application)
+
+(defn -main [& args]
+  (Application/launch clojure-javafxml-example.core args))
+
+(defn -start [this stage]
+  (let [loc (clojure.java.io/resource "window.fxml")
+        root (FXMLLoader/load loc)]
+
+    (.setScene stage (Scene. root))
+    (.setTitle stage "JavaFXML with Clojure Example")
+    (.show stage)))
