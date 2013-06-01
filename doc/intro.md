db0f6177f338f2b0114c70b21b36af1dbecc0cb0
coldnew <coldnew.tw@gmail.com>
2013-06-01 13:53:49 +0800

    add skenleton code

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-javafxml-example
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clojure_javafxml_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-javafxml-example.core-test
+  (:use clojure.test
+        clojure-javafxml-example.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
